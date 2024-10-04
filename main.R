d2b437638098eba589497bbd84882f1896cd4c4b
Devin Reed <reed.devin@gmail.com>
2024-10-04 16:03:38 +1300

    Added some pie plots for demographics. Still WIP.

--- main.R
+++ main.R
@@ -1,4 +1,6 @@
 library(dplyr)
+library(ggpie)
+library(ggplot2)
 
 source("datetime_string_fns.R")
 
@@ -12,51 +14,6 @@ source("datetime_string_fns.R")
 ################
 
 source("summarize_subjects.R")
-
-# TODO: figure out when I should use this, and exclude deployments with no observations?
-deployments_with_subjects <- deployments %>%
-  right_join(subjects_by_deployment, by = "Deployment.id")
-
-# TODO: and when I should use this, and include deployments with no observations?
-subjects_by_deployment <- deployments %>%
- left_join(subjects_by_deployment, by = "Deployment.id")
-
-########################
-# 1. Puma Demographics #
-########################
-
-pumas <- deployments_with_subjects %>%
-  group_by(Puma.name) %>%
-  summarize(N.kittens = max(N.kittens), .groups = 'drop') %>%
-  mutate(Reproductive.status = ifelse(
-    grepl("M", Puma.name),
-    "Solitary Male",
-    ifelse(
-      N.kittens == 0,
-      "Solitary Female",
-      "Female with Kittens"
-    )))
-
-count_repro_status <- pumas %>%
-  group_by(Reproductive.status) %>%
-  summarize(Count = n(), .groups = 'drop')
-
-ggplot(count_repro_status, aes(x = Reproductive.status, y = Count)) +
-  geom_bar(stat = "identity") +
-  labs(title = "Pumas by reproductive status",
-       x = "Reproductive status",
-       y = "Number of puma sampled") +
-  theme_minimal() +
-  theme(legend.position = "none") +
-  geom_text(aes(label = Count), 
-            position = position_stack(vjust = 1),
-            vjust = -0.45,
-            color = "black")
-
-
-# TODO: make some graphs about presence of different species at kills
-
-
 
 ############################################
 # Make Shapefile with all deployment stats #
--- make_figures.R
+++ make_figures.R
@@ -40,7 +40,75 @@ ggplot(observations_by_puma, aes(x = Puma.name, y = Deployments, fill = Behaviou
             color = "black")
 
 
-################################################
-# 2. Pie charts: summarize deployment subjects #
-################################################
+###################################
+# 2. Pie Chart: Puma Demographics #
+###################################
 
+deployments_with_subjects <- deployments %>%
+  right_join(subjects_by_deployment, by = "Deployment.id")
+
+pumas <- deployments_with_subjects %>%
+  group_by(Puma.name) %>%
+  summarize(N.kittens = max(N.kittens), .groups = 'drop') %>%
+  mutate(Reproductive.status = ifelse(
+    grepl("M", Puma.name),
+    "Solitary Male",
+    ifelse(
+      N.kittens == 0,
+      "Solitary Female",
+      "Female with Kittens"
+    )))
+
+ggpie(data = pumas, 
+      group_key = "Reproductive.status", 
+      count_type = "full",
+      fill_color = c("Solitary Male" = "#FF8C00", "Solitary Female" = "#1D5C3F", "Female with Kittens" = "#2E8B57"),
+      label_color = "white",
+      label_info = "count",
+      label_type = "horizon",
+      label_split = NULL,
+      label_size = 4.5, 
+      label_pos = "in") +
+  theme(legend.title = element_blank())
+
+
+####################################
+# 3. Pie Charts: Prey Demographics #
+####################################
+
+prey_demographics <- deployments %>%
+  select(Deployment.id, Puma.name, Carcass.species, Carcass.age.sex) %>%
+  mutate(Carcass.species = ifelse(tolower(trimws(Carcass.species)) == "mule deer", "Mule Deer", Carcass.species)) %>%
+  mutate(Carcass.age.sex = tolower(trimws(Carcass.age.sex)))
+
+ggpie(data = prey_demographics, 
+      group_key = "Carcass.species", 
+      count_type = "full",
+      border_size = 0.5,
+      fill_color = c("Mule Deer" = "#FF8C00", "Elk" = "#1D5C3F", "NA" ),
+      label_color = "black",
+      label_info = "count",
+      label_type = "horizon",
+      label_split = NULL,
+      label_size = 4.5, 
+      label_pos = "in",
+      label_threshold = 10) +
+  theme(legend.position = "inside",
+        legend.position.inside = c(0.85, 0.5),
+        legend.title = element_blank())
+
+ggpie(data = prey_demographics %>% filter(Carcass.species == "Mule Deer"), 
+      group_key = "Carcass.age.sex", 
+      count_type = "full",
+      border_size = 0.5,
+      #fill_color = c("Mule Deer" = "#FF8C00", "Elk" = "#1D5C3F", "NA" ),
+      label_color = "black",
+      label_info = "count",
+      label_type = "horizon",
+      label_split = NULL,
+      label_size = 4.5, 
+      label_pos = "in",
+      label_threshold = 10) +
+  theme(legend.position = "inside",
+        legend.position.inside = c(0.85, 0.5),
+        legend.title = element_blank())
